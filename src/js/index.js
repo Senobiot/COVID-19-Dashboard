0bf5f55aa56d065da83c24c34a6473da5fe31d21
Alex Kulik <inter_net@tut.by>
2020-12-10 18:11:43 +0300

    fix: clear index.js

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,8 @@ import '../../node_modules/leaflet/dist/leaflet'
 import Chart from '../../node_modules/chart.js/dist/Chart';
 import './header';
 import './table_total';
-//import './graph_1';
 import './country_panel.js';
 import './map';
-
-// import './covid';
 
 
 
