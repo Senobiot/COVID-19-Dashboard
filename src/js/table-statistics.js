999e0ba1154f3463b1e784e2bfebbd00b05f0772
sanazez <sanazez1234@gmail.com>
2020-12-21 15:15:41 +0300

    fix: delete console.log

--- src/js/table-statistics.js
+++ src/js/table-statistics.js
@@ -62,10 +62,6 @@ export default class CreateStatistics {
   }
 
   generateListStatistics(data, population, isTotal) {
-<<<<<<< HEAD
-=======
-    // console.log(data, population, isTotal);
->>>>>>> 6c211eff5af906c6f022871633b996e6385b95ab
     let additionalNumber = 0;
     if (isTotal) {
       additionalNumber = 6;
@@ -138,10 +134,6 @@ export default class CreateStatistics {
   async getTotalDataContinents() {
     const response = await fetch('https://disease.sh/v3/covid-19/continents?yesterday=false&twoDaysAgo=false');
     this.totalDataContinents = await response.json();
-<<<<<<< HEAD
-=======
-    // console.log(this.totalDataContinents);
->>>>>>> 6c211eff5af906c6f022871633b996e6385b95ab
   }
 
   async createContinents() {
