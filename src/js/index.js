39fd0bce5b65c92e4a6f911a968140c548783e1f
HannaHerman <mrs.hanna.herman@gmail.com>
2020-12-23 14:53:38 +0300

    fix: fix flags, buttons on map

--- src/js/index.js
+++ src/js/index.js
@@ -91,7 +91,7 @@ const addHandlerClickMap = () => {
         return;
       }
     }
-    if (targetElement.tagName === 'SPAN') {
+    if (targetElement.tagName === 'DIV') {
       const elIso2 = targetElement.dataset.iso2;
       if (elIso2) {
         worldMap.changeView(elIso2);
