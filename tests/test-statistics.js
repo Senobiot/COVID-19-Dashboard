f837da521f10bfbdd8cea5c2323c0312cdfcfbd2
sanazez <sanazez1234@gmail.com>
2020-12-24 22:17:54 +0300

    feat: added test-statistics

--- tests/test-statistics.js
+++ tests/test-statistics.js
@@ -1,19 +1,30 @@
-/* eslint-disable prefer-destructuring */
-/* eslint-disable import/no-extraneous-dependencies */
+/* run command */
+/* npm run test tests/test-statistics.js */
 require = require('esm')(module/* , options */);
 const mocha = require('mocha');
 const chai = require('chai');
 const create = require('./index');
 
-const expect = chai.expect;
+const { expect } = chai;
 
-describe('Reverse String Test', () => {
-  it('Checks if the strings is reversed', () => {
-    console.log(create);
+describe('Break the number into triads', () => {
+  it('Checks if a string is a triad', () => {
     const str1 = create.default.transformNumbers(17395);
     const str2 = create.default.transformNumbers(631435);
-
+    const str3 = create.default.transformNumbers(79219657);
     expect(str1).to.equal('17 395');
     expect(str2).to.equal('631 435');
+    expect(str3).to.equal('79 219 657');
+  });
+});
+
+describe('Calculation of indicators per hundred thousand', () => {
+  it('checking the correctness of calculations', () => {
+    const str1 = create.default.getPerPopulation(789, 1000000);
+    const str2 = create.default.getPerPopulation(631435, 25567123);
+    const str3 = create.default.getPerPopulation(543789, 47986234);
+    expect(str1).to.equal(78.9);
+    expect(str2).to.equal(2469.71);
+    expect(str3).to.equal(1133.22);
   });
 });
